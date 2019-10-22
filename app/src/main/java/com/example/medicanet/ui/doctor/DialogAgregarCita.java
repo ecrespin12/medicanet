87c47d7bd6b3625707ee24c4e959569aae9fad9f
daviddlopezz <46541007+daviddlopezz@users.noreply.github.com>
2019-10-22 00:03:45 -0600

    agregando Doctor Consultas

--- app/src/main/java/com/example/medicanet/ui/doctor/DialogAgregarCita.java
+++ app/src/main/java/com/example/medicanet/ui/doctor/DialogAgregarCita.java
@@ -9,10 +9,30 @@ import androidx.fragment.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.medicanet.R;
+import com.example.medicanet.metodos.MetodosDavid;
 
 public class DialogAgregarCita extends DialogFragment {
+
+    Spinner spTipoCita;
+
+    EditText edtDescripcion;
+    EditText edtFecha;
+    EditText edtHora;
+
+    ImageView imgCerrar;
+
+    Button btnGuardar;
+
+    ImageButton btnFecha;
+    ImageButton btnHora;
 
 
     public DialogAgregarCita() {
@@ -29,7 +49,46 @@ public class DialogAgregarCita extends DialogFragment {
         getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setCancelable(false);
 
+        //Enlazando vistas
+        spTipoCita = view.findViewById(R.id.spTipo_dialog_doc_agregar_cita);
+        edtDescripcion=view.findViewById(R.id.edtDescripcion_dialog_doc_agregar_cita);
+        edtFecha=view.findViewById(R.id.edtFecha_dialog_doc_agregar_cita);
+        edtHora=view.findViewById(R.id.edtHora_dialog_doc_agregar_cita);
+        imgCerrar=view.findViewById(R.id.imgCerrar_dialog_doc_agregar_cita);
+        btnGuardar=view.findViewById(R.id.btnGuardar_doc_modal_agregar_cita);
+        btnFecha=view.findViewById(R.id.btnFecha_dialog_doc_agregar_cita);
+        btnHora=view.findViewById(R.id.btnHora_dialog_doc_agregar_cita);
 
+        edtHora.setEnabled(false);
+        edtFecha.setEnabled(false);
+
+        imgCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        btnFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MetodosDavid.fecha(getContext(),edtFecha);
+            }
+        });
+
+        btnHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MetodosDavid.hora(getContext(),edtHora);
+            }
+        });
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Guardando...",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/example/medicanet/ui/doctor/datosPaciente/DatosPaciente.java
@@ -0,0 +1,29 @@
+package com.example.medicanet.ui.doctor.datosPaciente;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.medicanet.R;
+
+public class DatosPaciente extends Fragment {
+
+
+    public DatosPaciente() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_doc_datos_paciente, container, false);
+    }
+
+}
