00790435e9d908ffdc96944c2a89bcf8309eda03
daviddlopezz <46541007+daviddlopezz@users.noreply.github.com>
2019-10-22 02:02:53 -0600

    flujo de modales

---
+++ app/src/main/java/com/example/medicanet/ui/doctor/consultas/Consulta.java
@@ -0,0 +1,75 @@
+package com.example.medicanet.ui.doctor.consultas;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.medicanet.R;
+import com.example.medicanet.ui.doctor.DialogAgregarCita;
+import com.example.medicanet.ui.doctor.DialogAgregarDetalleConsulta;
+import com.example.medicanet.ui.doctor.DialogAgregarMedicamentoConsulta;
+
+public class Consulta extends Fragment {
+
+    Button btnDetalles;
+    Button btnAgregarCita;
+    Button btnMedicamentos;
+    Button btnTerminar;
+
+    public Consulta() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_doc_consulta, container, false);
+
+        //codigo agregado
+        btnDetalles=view.findViewById(R.id.btnDetalles_fragment_doc_consulta);
+        btnAgregarCita=view.findViewById(R.id.btnAgregarCita_fragment_doc_consulta);
+        btnMedicamentos=view.findViewById(R.id.btnMedicamentos_fragment_doc_consulta);
+        btnTerminar=view.findViewById(R.id.btnTerminar_fragment_doc_consulta);
+
+        btnAgregarCita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dialog/modal a invocar
+                DialogAgregarCita dialog = new DialogAgregarCita();
+                dialog.show(getFragmentManager(), "dialog_admin_ejemplo");
+            }
+        });
+        btnDetalles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogAgregarDetalleConsulta dialog = new DialogAgregarDetalleConsulta();
+                dialog.show(getFragmentManager(),"dialog_doc_agregar_detalle");
+            }
+        });
+        btnMedicamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogAgregarMedicamentoConsulta dialog = new DialogAgregarMedicamentoConsulta();
+                dialog.show(getFragmentManager(),"dialog_doc_agregar_medicamento");
+            }
+        });
+        btnTerminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //fin codigo agregado
+
+        return  view;
+    }
+
+}
--- app/src/main/java/com/example/medicanet/ui/doctor/datosPaciente/DatosPaciente.java
+++ app/src/main/java/com/example/medicanet/ui/doctor/datosPaciente/DatosPaciente.java
@@ -4,15 +4,20 @@ package com.example.medicanet.ui.doctor.datosPaciente;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.medicanet.R;
+import com.example.medicanet.ui.doctor.consultas.Consulta;
 
 public class DatosPaciente extends Fragment {
 
+    Button btnHistorial;
+    Button btnIniciarConsulta;
 
     public DatosPaciente() {
         // Required empty public constructor
@@ -22,8 +27,37 @@ public class DatosPaciente extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doc_datos_paciente, container, false);
+        View view = inflater.inflate(R.layout.fragment_doc_datos_paciente, container, false);
+
+        //codigo agregado
+
+        btnHistorial=view.findViewById(R.id.btnHistorial_fragment_doc_datos_paciente);
+        btnIniciarConsulta=view.findViewById(R.id.btnIniciarConsulta_fragment_doc_datos_paciente);
+
+        btnHistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnIniciarConsulta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Consulta detalle=new Consulta();
+
+                // Crea el nuevo fragmento y la transacción.
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, detalle);
+                transaction.addToBackStack(null);
+
+                // Commit a la transacción
+                transaction.commit();
+            }
+        });
+        //fin codigo agregado
+        return view;
     }
 
 }
