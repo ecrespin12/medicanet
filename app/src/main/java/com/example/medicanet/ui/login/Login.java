d52074c0d222a9cd4aa80ca93612856d403aff75
ecrespin12 <ecrespin.612@gmail.com>
2019-10-22 04:39:31 -0600

    Referencias a Firebase
    Conexion con firebase

--- app/src/main/java/com/example/medicanet/ui/login/Login.java
+++ app/src/main/java/com/example/medicanet/ui/login/Login.java
@@ -1,22 +1,35 @@
 package com.example.medicanet.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.medicanet.MainActivity;
 import com.example.medicanet.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
     Button btn_iniciar,btn_salir;
     EditText edtUser;
-
+    EditText edtUsuario, edtClave;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private ValueEventListener eventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,5 +68,46 @@ public class Login extends AppCompatActivity {
                 finishAffinity();
             }
         },100);
+    }
+
+
+    public void login(View v){
+
+
+        final String email = edtUsuario.getText().toString().trim();
+        String password = edtClave.getText().toString().trim();
+
+        //hay que identificarse en firebase auth y si el auth es exitoso buscamos los datos de usuario por keyID
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Inicio exitoso
+
+                            //objeto usuario que recupera el ID de usuario actual
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String idUser = user.getUid();
+                            //obtenerRolUsuario(idUser);
+
+                            //Intent i = new Intent(getApplicationContext(), MenuPrincipal.class);
+                            //startActivity(i);
+
+                            //obtenerRol(email);
+                            //Toast.makeText(getApplicationContext(), "Bienvenido" + y, Toast.LENGTH_LONG).show();
+
+                        } else {
+                            // si falla mostrar.
+                            Log.w("Fallo", "Error al iniciar", task.getException());
+                            Toast.makeText(getApplicationContext(), "Auth fallo.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+
     }
 }
