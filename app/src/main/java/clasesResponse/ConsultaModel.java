d4541c8aa15af06946d10c89e1a0107ddaf2b14e
Oscar Torres <40122963+JossueTorres@users.noreply.github.com>
2019-11-09 11:33:20 -0600

    Cambiar campos del modelo a string solo para probar json recived

--- app/src/main/java/clasesResponse/ConsultaModel.java
+++ app/src/main/java/clasesResponse/ConsultaModel.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class ConsultaModel {
     @SerializedName("cme_codigo")
-    public int cme_codigo;
+    public String cme_codigo;
     @SerializedName("per_nombre")
     public String per_nombre;
     @SerializedName("per_correo")
@@ -14,19 +14,19 @@ public class ConsultaModel {
     @SerializedName("per_dui")
     public String per_dui;
     @SerializedName("per_fecha_nace")
-    public Date per_fecha_nace;
+    public String per_fecha_nace;
     @SerializedName("med_nombre")
     public String med_nombre;
     @SerializedName("med_correo")
     public String med_correo;
     @SerializedName("cmd_codigo")
-    public int cmd_codigo;
+    public String cmd_codigo;
     @SerializedName("cmd_nombre")
     public String cmd_nombre;
     @SerializedName("cmd_latitud")
-    public double cmd_latitud;
+    public String cmd_latitud;
     @SerializedName("cmd_longitud")
-    public double cmd_longitud;
+    public String cmd_longitud;
     @SerializedName("cme_fecha_hora")
-    public Date cme_fecha_hora;
+    public String cme_fecha_hora;
 }
--- app/src/main/java/com/example/medicanet/ui/doctor/consultas/ConsultasProgramadas.java
+++ app/src/main/java/com/example/medicanet/ui/doctor/consultas/ConsultasProgramadas.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Handler;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
