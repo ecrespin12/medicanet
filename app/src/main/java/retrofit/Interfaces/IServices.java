6782456aaefcd38d7b4f28e8dbc936550a01c3e9
Zirok Guadron <zirokguadron11@gmail.com>
2019-11-17 21:59:10 -0600

    consumiendo medicamentos pendientes

--- app/src/main/java/retrofit/Interfaces/IServices.java
+++ app/src/main/java/retrofit/Interfaces/IServices.java
@@ -3,16 +3,20 @@ package retrofit.Interfaces;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Objects;
 
+<<<<<<< Updated upstream
+=======
+import clasesResponse.CentroMedicoModel;
+import clasesResponse.ConsultaDetalleModel;
+import clasesResponse.ConsultaModel;
+import clasesResponse.HistorialModel;
+import clasesResponse.MedicamentosPendientesModel;
+import clasesResponse.MedicamentosModel;
+>>>>>>> Stashed changes
 import clasesResponse.clItems;
 import clasesResponse.clPrueba;
-import clasesResponse.clVersion;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
 import retrofit2.http.Query;
 
 public interface IServices {
@@ -24,4 +28,35 @@ public interface IServices {
     @NotNull
     Call<clItems> getConsultaVersion(@Query("id") @NotNull String id);
 
+<<<<<<< Updated upstream
+=======
+    @GET("CentroMedico/centro_medico")
+    @NotNull
+    Call<List<CentroMedicoModel>> getCentroMedico();
+
+    @GET("Medicamentos/catalogo")
+    @NotNull
+    Call<List<MedicamentosModel>> getMedicamentos();
+
+    @GET("Consulta/consulta?")
+    @NotNull
+    Call<List<ConsultaModel>> getConsultas(@Query("per") int per, @Query("doc") int doc, @Query("cod") int cod);
+
+    @GET("Consulta/detalle?")
+    @NotNull
+    Call<List<ConsultaDetalleModel>> getConsultasDetalle(@Query("cod") int cod);
+
+    @GET("Medicos/fechaEstaLibre?")
+    @NotNull
+    Call<Boolean> getFechaEstaLibre(@Query("fec") int fec, @Query("med") int med);
+
+    @GET("Historial/historial?")
+    @NotNull
+    Call<List<HistorialModel>> getHistorialPaciente(@Query("per") @NotNull int per);
+
+    @GET("Medicamentos/medicamentos_pendientes?")
+    @NotNull
+    Call<List<MedicamentosPendientesModel>> getMedicamentosPendientes(@Query("cme") int cme, @Query("per") int per);
+
+>>>>>>> Stashed changes
 }
