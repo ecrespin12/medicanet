e9b8134c81b991fae4b86fbab77f3786a5148839
daviddlopezz <46541007+daviddlopezz@users.noreply.github.com>
2019-11-28 03:58:46 -0600

    1

--- app/src/main/java/com/example/medicanet/ui/doctor/dialogs/DialogAgregarCita.java
+++ app/src/main/java/com/example/medicanet/ui/doctor/dialogs/DialogAgregarCita.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.DialogFragment;
 
 import android.os.Handler;
 import android.util.Half;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,14 +23,31 @@ import android.widget.Toast;
 import com.example.medicanet.R;
 import com.example.medicanet.metodos.AdaptadorSpinner;
 import com.example.medicanet.metodos.Metodos;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import clasesResponse.CentroMedicoModel;
 import clasesResponse.ConsultaModel;
+import retrofit.Interfaces.IServices;
+import retrofit.RetrofitClientInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DialogAgregarCita extends DialogFragment {
 
-    Spinner             spTipoCita;
+    RetrofitClientInstance ret = new RetrofitClientInstance();
+    private IServices servicio;
+    List<CentroMedicoModel> centroMedicoModels;
+    CentroMedicoModel centroMedicoModel;
+
+    Spinner             spCentro;
     AdaptadorSpinner    adaptadorSpinner;
 
     EditText            edtDescripcion;
@@ -43,9 +61,13 @@ public class DialogAgregarCita extends DialogFragment {
     ImageButton         btnFecha;
     ImageButton         btnHora;
 
-    TypedArray          imagenes;
-    String []           nombres;
-    String []           descripciones;
+    int codigoMedicamento=0;
+    int codigoConsulta=0;
+
+    String [] arr1;
+    String [] arr2;
+    String [] arr3;
+    String [] arr4;
 
     ConsultaModel item;
 
@@ -60,12 +82,15 @@ public class DialogAgregarCita extends DialogFragment {
         View view = inflater.inflate(R.layout.dialog_doc_agregar_cita, container, false);
 
         //CODIGO AGREGADO
+
+        //INICIALIZAR OBJETO DE LA INTERFAZ
+        servicio = (IServices) ret.createService(IServices.class, getContext().getResources().getString(R.string.token));
+
         getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setCancelable(false);
 
         //Enlazando vistas
-        spTipoCita = view.findViewById(R.id.spTipo_dialog_doc_agregar_cita);
-        edtDescripcion=view.findViewById(R.id.edtDescripcion_dialog_doc_agregar_cita);
+        spCentro = view.findViewById(R.id.spTipo_dialog_doc_agregar_cita);
         edtFecha=view.findViewById(R.id.edtFecha_dialog_doc_agregar_cita);
         edtHora=view.findViewById(R.id.edtHora_dialog_doc_agregar_cita);
         btnCerrar=view.findViewById(R.id.imgCerrar_dialog_doc_agregar_cita);
@@ -73,15 +98,11 @@ public class DialogAgregarCita extends DialogFragment {
         btnFecha=view.findViewById(R.id.btnFecha_dialog_doc_agregar_cita);
         btnHora=view.findViewById(R.id.btnHora_dialog_doc_agregar_cita);
 
-        imagenes=getResources().obtainTypedArray(R.array.img_item_list_ejemplo);
-        nombres=getResources().getStringArray(R.array.campo1_item_list_ejemplo);
-        descripciones=getResources().getStringArray(R.array.campo2_item_list_ejemplo);
-
-        adaptadorSpinner = new AdaptadorSpinner(getContext(),imagenes,nombres,descripciones,null,null);
-        spTipoCita.setAdapter(adaptadorSpinner);
 
         edtHora.setEnabled(false);
         edtFecha.setEnabled(false);
+
+        getCentros();
 
         btnFecha.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,5 +172,46 @@ public class DialogAgregarCita extends DialogFragment {
         });
 
         return view;
+    }
+
+    //METODO PARA CONSUMIR EL WS
+    private void getCentros() {
+
+        Log.d("JTDebug", "Entra Metodo getCentros");
+        Call<List<CentroMedicoModel>> call = servicio.getCentroMedico();
+        Log.d("JTDebug", "Url: " + ret.BASE_URL);
+        call.enqueue(new Callback<List<CentroMedicoModel>>() {
+            @Override
+            public void onResponse(Call<List<CentroMedicoModel>> call, Response<List<CentroMedicoModel>> response) {
+                Log.d("JTDebug", "Entra OnResponse");
+                try {
+                    if (response.isSuccessful()) {
+                        Log.d("JTDebug", "Entra IsSuccessful");
+                        centroMedicoModels = response.body();
+                        Log.d("JTDebug", "Count: " + centroMedicoModels.size());
+
+                        arr1=new String[centroMedicoModels.size()];
+
+                        for (int i=0;i<centroMedicoModels.size();i++) {
+                            centroMedicoModel = centroMedicoModels.get(i);
+                            arr1[i] = "Código: "+centroMedicoModel.cmd_nombre;
+                        }
+                        AdaptadorSpinner adaptadorSpinner = new AdaptadorSpinner(getContext(), null, arr1, null, null, null);
+                        spCentro.setAdapter(adaptadorSpinner);
+                    } else {
+                        Log.d("JTDebug", "Entra not Successful. Code: " + response.code() + "\nMessage: " + response.message());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CentroMedicoModel>> call, Throwable t) {
+                Log.d("JTDebug", "Entra OnFailure");
+                Log.d("JTDebug", "Message: " + t.getMessage());
+                t.printStackTrace();
+            }
+        });
     }
 }
